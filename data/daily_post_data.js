4c44da5562505124bbde62881be3af25c049f15f
Corbeau <aurora.griffith@students.mq.edu.au>
2025-09-05 09:17:32 +1000

    adding image files and post data

---
+++ data/daily_post_data.js
@@ -0,0 +1,481 @@
+const DAILY_POST_DATA = {
+    // data about data
+    "metadata": [
+        // ========================================
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/12/06",
+            // the theme that post is for
+            "theme": "free flowing",
+            // the day that post is for
+            "day": "friday",
+            // the name for the post on that day
+            "title": "files follow format",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/12/05",
+            // the theme that post is for
+            "theme": "thundering thicket",
+            // the day that post is for
+            "day": "thursday",
+            // the name for the post on that day
+            "title": "these theoretical thoughtfully things",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/12/04",
+            // the theme that post is for
+            "theme": "wdym",
+            // the day that post is for
+            "day": "wednesday",
+            // the name for the post on that day
+            "title": "ambitious animation architecture",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/12/03",
+            // the theme that post is for
+            "theme": "scheming",
+            // the day that post is for
+            "day": "tuesday",
+            // the name for the post on that day
+            "title": "lorem ipsum kind of day",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/12/02",
+            // the theme that post is for
+            "theme": "moony",
+            // the day that post is for
+            "day": "monday",
+            // the name for the post on that day
+            "title": "watch the moon glow",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/12/01",
+            // the theme that post is for
+            "theme": "situation",
+            // the day that post is for
+            "day": "sunday",
+            // the name for the post on that day
+            "title": "the suffering of VAOs",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/30",
+            // the theme that post is for
+            "theme": "cleanup",
+            // the day that post is for
+            "day": "saturday",
+            // the name for the post on that day
+            "title": "your regular program has been hijacked by the shape's gang",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/29",
+            // the theme that post is for
+            "theme": "free flowing",
+            // the day that post is for
+            "day": "friday",
+            // the name for the post on that day
+            "title": "crate-ly appreciated",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/28",
+            // the theme that post is for
+            "theme": "thundering thicket",
+            // the day that post is for
+            "day": "thursday",
+            // the name for the post on that day
+            "title": "minimalism helps sometimes",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/27",
+            // the theme that post is for
+            "theme": "wdym",
+            // the day that post is for
+            "day": "wednesday",
+            // the name for the post on that day
+            "title": "cooking the books",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/26",
+            // the theme that post is for
+            "theme": "scheming",
+            // the day that post is for
+            "day": "tuesday",
+            // the name for the post on that day
+            "title": "reading time",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/25",
+            // the theme that post is for
+            "theme": "moony",
+            // the day that post is for
+            "day": "monday",
+            // the name for the post on that day
+            "title": "clouds are- oh look a butterfly",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/24",
+            // the theme that post is for
+            "theme": "situation",
+            // the day that post is for
+            "day": "sunday",
+            // the name for the post on that day
+            "title": "we've scene a stage",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/23",
+            // the theme that post is for
+            "theme": "cleanup",
+            // the day that post is for
+            "day": "saturday",
+            // the name for the post on that day
+            "title": "chill transparency",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/22",
+            // the theme that post is for
+            "theme": "free flowing",
+            // the day that post is for
+            "day": "friday",
+            // the name for the post on that day
+            "title": "it's an art project",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/21",
+            // the theme that post is for
+            "theme": "thundering thicket",
+            // the day that post is for
+            "day": "thursday",
+            // the name for the post on that day
+            "title": "stormy seas",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/20",
+            // the theme that post is for
+            "theme": "wdym",
+            // the day that post is for
+            "day": "wednesday",
+            // the name for the post on that day
+            "title": "puzzling thoughts",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/19",
+            // the theme that post is for
+            "theme": "scheming",
+            // the day that post is for
+            "day": "tuesday",
+            // the name for the post on that day
+            "title": "seeming snooze-day",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/18",
+            // the theme that post is for
+            "theme": "moony",
+            // the day that post is for
+            "day": "monday",
+            // the name for the post on that day
+            "title": "wavy water",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/17",
+            // the theme that post is for
+            "theme": "situation",
+            // the day that post is for
+            "day": "sunday",
+            // the name for the post on that day
+            "title": "sun lighting",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/16",
+            // the theme that post is for
+            "theme": "cleanup",
+            // the day that post is for
+            "day": "saturday",
+            // the name for the post on that day
+            "title": "WIP",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/15",
+            // the theme that post is for
+            "theme": "free flowing",
+            // the day that post is for
+            "day": "friday",
+            // the name for the post on that day
+            "title": "shapes and colours",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/14",
+            // the theme that post is for
+            "theme": "thundering thicket",
+            // the day that post is for
+            "day": "thursday",
+            // the name for the post on that day
+            "title": "captain's log, 2024/11/14,",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/13",
+            // the theme that post is for
+            "theme": "wdym",
+            // the day that post is for
+            "day": "wednesday",
+            // the name for the post on that day
+            "title": "major arcana / minor arcana",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/12",
+            // the theme that post is for
+            "theme": "scheming",
+            // the day that post is for
+            "day": "tuesday",
+            // the name for the post on that day
+            "title": "conventional structure",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/11",
+            // the theme that post is for
+            "theme": "moony",
+            // the day that post is for
+            "day": "monday",
+            // the name for the post on that day
+            "title": "complex time suffering",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/10",
+            // the theme that post is for
+            "theme": "situation",
+            // the day that post is for
+            "day": "sunday",
+            // the name for the post on that day
+            "title": "new posting meta",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/09",
+            // the theme that post is for
+            "theme": "cleanup",
+            // the day that post is for
+            "day": "saturday",
+            // the name for the post on that day
+            "title": "post importing",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/08",
+            // the theme that post is for
+            "theme": "free flowing",
+            // the day that post is for
+            "day": "friday",
+            // the name for the post on that day
+            "title": "shapes, extrusions, worms, and NURBS",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/07",
+            // the theme that post is for
+            "theme": "thundering thicket",
+            // the day that post is for
+            "day": "thursday",
+            // the name for the post on that day
+            "title": "noisy canopys",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/06",
+            // the theme that post is for
+            "theme": "wdym",
+            // the day that post is for
+            "day": "wednesday",
+            // the name for the post on that day
+            "title": "spooky skeletons",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/11/05",
+            // the theme that post is for
+            "theme": "scheming",
+            // the day that post is for
+            "day": "tuesday",
+            // the name for the post on that day
+            "title": "daily post idea",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+        // ========================================
+        // ...
+        // ========================================
+    ],
+    // moving this out of the post_meta list to remove
+    //  the need for filtering/testing that we're not
+    //  generating a stub for it by accident
+    "template": [
+        // putting it in a list to make it
+        //  easier to copy paste
+        {
+            // date of the post in YYYY/MM/DD format
+            "date": "2024/12/31",
+            // the theme that post is for
+            "theme": "lorem ipsum",
+            // the day that post is for
+            "day": "lorem ipsum",
+            // the name for the post on that day
+            "title": "lorem ipsum",
+            // this is just always the book for now
+            "thumbnail": "/img/bookicon.png",
+            "thumbnail_alt_text": "an open book icon"
+        },
+    ],
+};
+
+export {
+    DAILY_POST_DATA
+};
